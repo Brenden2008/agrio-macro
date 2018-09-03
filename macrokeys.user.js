b5677dd5ffd87aa74a1c7484b4bbedf4101cf816
Brenden2008 <42918416+Brenden2008@users.noreply.github.com>
2018-09-03 14:49:56 -0500

    Create macrokeys.user.js

---
+++ macrokeys.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    https://github.com
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *.agar.io/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();

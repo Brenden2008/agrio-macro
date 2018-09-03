96dc8d9c30f1ad88e30bebf9345be87e5d502067
Brenden2008 <42918416+Brenden2008@users.noreply.github.com>
2018-09-03 15:08:08 -0500

    Update macrokeys.user.js

--- macrokeys.user.js
+++ macrokeys.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         agrio-macro
 // @namespace    https://github.com
 // @version      0.1
 // @description  try to take over the world!
@@ -11,5 +11,55 @@
 (function() {
     'use strict';
 
-    // Your code here...
+    /*--- Get or set the codes for the arrow keys.
+    Firefox gives us nice constants, Chrome does not.
+*/
+KeyEvent        = (typeof KeyEvent === "object")  ?  KeyEvent  :  [];
+const a  = KeyEvent.DOM_VK_LEFT   ||  65;
+const RIGHT_KEY = KeyEvent.DOM_VK_RIGHT  ||  39;
+
+window.addEventListener ("keydown", keyboardHandler, false);
+
+function keyboardHandler (zEvent) {
+    var bBlockDefaultAction = false;
+
+    //--- Assume we want only the plain keys, not the modified versions.
+    if (zEvent.altKey  ||  zEvent.ctrlKey  ||  zEvent.shiftKey) {
+        //-- Do nothing (most user-friendly option, in most cases).
+    }
+    else {
+        if (zEvent.which == a) {
+             var e = new Event("keydown");
+  e.key="w";    // just enter the char you want to send 
+  e.keyCode=e.key.charCodeAt(0);
+  e.which=e.keyCode;
+  e.altKey=false;
+  e.ctrlKey=true;
+  e.shiftKey=false;
+  e.metaKey=false;
+  e.bubbles=true;
+  document.dispatchEvent(e);
+             var e = new Event("keydown");
+  e.key="w";    // just enter the char you want to send 
+  e.keyCode=e.key.charCodeAt(0);
+  e.which=e.keyCode;
+  e.altKey=false;
+  e.ctrlKey=true;
+  e.shiftKey=false;
+  e.metaKey=false;
+  e.bubbles=true;
+  document.dispatchEvent(e);
+            bBlockDefaultAction = true;
+        }
+        else if (zEvent.which == RIGHT_KEY) {
+            //DO RIGHT KEY ACTION HERE.
+            bBlockDefaultAction = true;
+        }
+    }
+
+    if (bBlockDefaultAction) {
+        zEvent.preventDefault ();
+        zEvent.stopPropagation ();
+    }
+}
 })();
